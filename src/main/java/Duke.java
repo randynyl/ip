ab80930ae4f278fdbd774333874a91d0d849c8c8
randynyl <56857427+randynyl@users.noreply.github.com>
2020-08-26 21:15:24 +0800

    add mark done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,30 +3,36 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
         System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("\t> Good day, sire. I'm Percival");
+        System.out.println("\t> Good day, sire, Percival at your disposal.");
         System.out.println("\t> How may I be of service?");
         System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
         Scanner in = new Scanner(System.in);
         String userInput = in.nextLine();
-
-        while (!"bye".equals(userInput)) {
+        String userCommand = userInput.split(" ")[0];
+        while (!"bye".equals(userCommand)) {
             System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            if ("list".equals(userInput)) {
+            if ("list".equals(userCommand)) {
                 System.out.println("\t> Here are your tasks, sire: ");
                 for (int i=0; i<tasks.size(); i++) {
-                    System.out.println("\t" + (i+1) + ". " + tasks.get(i));
+                    System.out.println("\t" + (i+1) + "." + tasks.get(i).getTaskDetails());
                 }
+            } else if ("done".equals(userCommand)) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                tasks.get(taskNumber - 1).markAsDone();
+                System.out.println("\t> Very well. This task shall be marked as done: \n\t  "
+                        + tasks.get(taskNumber - 1).getTaskDetails());
             } else {
-                tasks.add(userInput);
+                Task newTask = new Task(userInput);
+                tasks.add(newTask);
                 System.out.println("\t> added: " + userInput);
             }
             System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             userInput = in.nextLine();
+            userCommand = userInput.split(" ")[0];
         }
-
         System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("\t> Goodbye, sire. Fare thee well.");
         System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        isDone = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String getTaskDetails() {
+        return "[" + getStatusIcon() + "] " + getTaskName();
+    }
+}
