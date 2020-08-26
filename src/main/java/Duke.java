f04eaa071e47ec46bf65996ac7f15e5747ceff0f
randynyl <56857427+randynyl@users.noreply.github.com>
2020-08-26 20:35:35 +0800

    Add list, add item functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-
+        ArrayList<String> tasks = new ArrayList<String>();
         System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("\t> Good day, sire. I'm Percival");
         System.out.println("\t> How may I be of service?");
@@ -13,7 +14,15 @@ public class Duke {
 
         while (!"bye".equals(userInput)) {
             System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            System.out.println("\t> " + userInput);
+            if ("list".equals(userInput)) {
+                System.out.println("\t> Here are your tasks, sire: ");
+                for (int i=0; i<tasks.size(); i++) {
+                    System.out.println("\t" + (i+1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(userInput);
+                System.out.println("\t> added: " + userInput);
+            }
             System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             userInput = in.nextLine();
         }
