98e3e25981502268bcd024fc82e84608150343bc
randynyl <56857427+randynyl@users.noreply.github.com>
2020-08-26 20:14:27 +0800

    Add greet, echo, exit functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
-        System.out.println("__________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("__________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("__________________________________________");
+        System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("\t> Good day, sire. I'm Percival");
+        System.out.println("\t> How may I be of service?");
+        System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+
+        while (!"bye".equals(userInput)) {
+            System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("\t> " + userInput);
+            System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            userInput = in.nextLine();
+        }
+
+        System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("\t> Goodbye, sire. Fare thee well.");
+        System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 }
