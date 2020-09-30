54b13312a024805bcd030cabb3d53dced3fc2eed
randynyl <56857427+randynyl@users.noreply.github.com>
2020-10-01 00:00:12 +0800

    Refine read file function

--- src/main/java/duke/IOManager.java
+++ src/main/java/duke/IOManager.java
@@ -91,8 +91,6 @@ public class IOManager {
             tasks.addAll(readFileContents(filePath));
         } catch (FileNotFoundException e) {
             File f = new File(filePath);
-//            File folder = new File("data");
-//            folder.mkdir();
             f.createNewFile();
         }
         return tasks;
